df5c25e8688e6c2df0cd2b3e295e3c8176fc23fc
HifzaSethi <hifzasethi@gmail.com>
2026-09-02 14:24:56 +0500

    Build separate data folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,5 @@
-import type { Product } from "@/types/product";
-const products: Product[] = [
-  {
-    name: "Zamurd",
-    price: 2500,
-    id: 1,
-    category: "kids",
-  },
-  {
-    name: "Rimjhim",
-    price: 2200,
-    id: 2,
-    category: "kids",
-  },
-  {
-    name: "Mahtab",
-    price: 3000,
-    id: 3,
-    category: "kids",
-  },
-];
+
+import { products } from "@/data/product";
 
 import ProductCard from "@/components/ProductCard";
 
---
+++ src/data/products.ts
@@ -0,0 +1,21 @@
+import type { Product } from "@/types/product";
+export const products: Product[] = [
+  {
+    name: "Zamurd",
+    price: 2500,
+    id: 1,
+    category: "kids",
+  },
+  {
+    name: "Rimjhim",
+    price: 2200,
+    id: 2,
+    category: "kids",
+  },
+  {
+    name: "Mahtab",
+    price: 3000,
+    id: 3,
+    category: "kids",
+  },
+];
